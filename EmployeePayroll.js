02c634862287f6058e5dbc95cca7d36d9d8aeb20
jangle <shubhamjangale0000@gmail.com>
2020-11-14 09:34:44 +0530

    Modified : Modified the function to calculate the Employee Wage for number of days

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -5,7 +5,7 @@ const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4
 const FULL_TIME_HOURS = 8
 const WAGE_PER_HOUR = 20
-
+const NO_OF_DAYS = 2
 function getWorkingHours(emp_check){
   switch(emp_check){
     case IS_PART_TIME:
@@ -16,7 +16,12 @@ function getWorkingHours(emp_check){
       return 0
   }
 }
-let emp_check = Math.floor(Math.random() * 10) % 3;
-let emp_hrs = getWorkingHours(emp_check);
-let daily_wage = emp_hrs * WAGE_PER_HOUR;
-console.log("Employee Wage is " + daily_wage);
+let total_Wage = 0
+let emp_hrs = 0
+for(let i = 0; i < NO_OF_DAYS; i++){
+  let emp_check = Math.floor(Math.random() * 10) % 3;
+  emp_hrs += getWorkingHours(emp_check);
+  
+}
+let total_wage = emp_hrs * WAGE_PER_HOUR;
+console.log("Employee Wage is " + total_wage);
