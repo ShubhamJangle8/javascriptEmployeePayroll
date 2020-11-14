525e482d05a95a348116a72412d5fde228e8c2ca
jangle <shubhamjangale0000@gmail.com>
2020-11-14 09:22:28 +0530

    Refactored : Refactored the code to change the logic to use function

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -5,18 +5,18 @@ const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4
 const FULL_TIME_HOURS = 8
 const WAGE_PER_HOUR = 20
-let emp_check = Math.floor(Math.random() * 10) % 3;
-let emp_hrs = 0;
-switch(emp_check){
-  case IS_PART_TIME:
-    emp_hrs = PART_TIME_HOURS;
-    break;
-  case IS_FULL_TIME:
-    emp_hrs = FULL_TIME_HOURS;
-    break;
-  default:
-    emp_hrs = 0
-    break;
+
+function getWorkingHours(emp_check){
+  switch(emp_check){
+    case IS_PART_TIME:
+      return PART_TIME_HOURS;
+    case IS_FULL_TIME:
+      return FULL_TIME_HOURS;
+    default:
+      return 0
+  }
 }
+let emp_check = Math.floor(Math.random() * 10) % 3;
+let emp_hrs = getWorkingHours(emp_check);
 let daily_wage = emp_hrs * WAGE_PER_HOUR;
-console.log(daily_wage);
+console.log("Employee Wage is " + daily_wage);
