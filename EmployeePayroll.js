49db4eab1bb08e449750bb67de670e742cc62d8f
jangle <shubhamjangale0000@gmail.com>
2020-11-14 08:56:03 +0530

    Added : Added ability to check the attendance of an Employee

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Employee Payroll Javascript")
+{
+const IS_ABSENT = 0;
+let emp_check = Math.floor(Math.random() / 10) % 2
+if(emp_check == IS_ABSENT)
+  console.log("Employee is absent...")
+else
+  console.log("Employee is present...")
+}
