c9880c63e0229b3716c6099a81e14f1869620d90
jangle <shubhamjangale0000@gmail.com>
2020-11-14 11:39:01 +0530

    Added : Added the different array operation methods to find outcome out of Array

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -6,8 +6,8 @@ const PART_TIME_HOURS = 4
 const FULL_TIME_HOURS = 8
 const WAGE_PER_HOUR = 20
 const NO_OF_DAYS = 2
-function getWorkingHours(emp_check){
-  switch(emp_check){
+function getWorkingHours(empCheck){
+  switch(empCheck){
     case IS_PART_TIME:
       return PART_TIME_HOURS;
     case IS_FULL_TIME:
@@ -16,23 +16,84 @@ function getWorkingHours(emp_check){
       return 0
   }
 }
-let daily_hrs = 0
-let total_hrs = 0
-let daily_wage = 0
-let total_wage = 0
-let working_days = 0
+let empCheck = 0
+let dailyHrs = 0
+let totalHrs = 0
+let dailyWage = 0
+let totalWage = 0
+let WorkingDays = 0
 let wages = new Array()
 
-function calcDailyWage(daily_hrs){
-  return daily_hrs * WAGE_PER_HOUR
+function calcDailyWage(dailyHrs){
+  return dailyHrs * WAGE_PER_HOUR
 }
-while(total_hrs <= 160 && working_days < 20){
-  working_days++;
-  let emp_check = Math.floor(Math.random() * 10) % 3;
-  daily_hrs = getWorkingHours(emp_check);
-  total_hrs += daily_hrs
-  daily_wage = calcDailyWage()
-  wages.push(daily_wage)
+
+while(totalHrs <= 160 && WorkingDays < 20){
+  WorkingDays++;
+  empCheck = Math.floor(Math.random() * 10) % 3;
+  dailyHrs = getWorkingHours(empCheck);
+  totalHrs += dailyHrs
+  dailyWage = calcDailyWage(dailyHrs)
+  wages.push(dailyWage)
 }
-total_wage = calcDailyWage(total_hrs)
-console.log("Employee Wage for " + working_days +" working days with " + total_hrs + " hours is " + total_wage);
+
+//UC7A
+function sumForEach(dailyWage){
+  totalWage += dailyWage;
+}
+wages.forEach(sumForEach);
+console.log("For Each, Employee Wage for " + WorkingDays +" working days with " + totalHrs + " hours is " + totalWage);
+
+function SumByReduce(totalWage, dailyWage){
+  return totalWage + dailyWage
+}
+wages.reduce(SumByReduce, 0)
+console.log("Reduce, Employee Wage for " + WorkingDays +" working days with " + totalHrs + " hours is " + totalWage);
+
+//UC7B
+let dailyCentr = 0
+function mapDayWithWage(dailyWage){
+  dailyCentr++;
+  return dailyCentr + "=" + dailyWage;
+}
+let mapDayWithWageArray = wages.map(mapDayWithWage)
+console.log("Day with wage array : ")
+console.log(mapDayWithWageArray)
+
+//UC7C
+function fullTimeWage(dailyWage){
+  return dailyWage.includes("160")
+}
+let fullTimeArray = mapDayWithWageArray.filter(fullTimeWage);
+console.log("Employees taking full time wages")
+console.log(fullTimeArray)
+
+//UC7D
+function fullTimeWage(dailyWage){
+  return dailyWage.includes("160")
+}
+let firstFullTime = mapDayWithWageArray.find(fullTimeWage)
+console.log("First full time Employee")
+console.log(firstFullTime)
+
+//UC7E
+function checkEachFullTime(dailyWage){
+  return dailyWage.includes("160")
+}
+console.log("Whether all Employees work full time : ");
+console.log(mapDayWithWageArray.every(checkEachFullTime))
+
+//UC7F
+function checkAnyPartTime(dailyWage){
+  return dailyWage.includes("80")
+}
+console.log("Whether any Part time Employees Present : ")
+console.log(mapDayWithWageArray.some(checkAnyPartTime))
+
+//UC7G
+function calculateWorkingDays(dailyWage){
+  return dailyWage.includes("80") || dailyWage.includes("160")
+}
+console.log("No Of days Employee Worked are ")
+let workingArray = mapDayWithWageArray.filter(calculateWorkingDays)
+console.log(workingArray.length)
