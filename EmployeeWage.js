9e322dc391ed2ec2c1f8d5bff296a20678787dc5
jangle <shubhamjangale0000@gmail.com>
2020-11-15 14:28:56 +0530

    Added : Ability to store Employee Wage data into object

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -16,49 +16,26 @@ function getWorkingHours(empCheck){
   }
 }
 let totalHrs = 0
-let WorkingDays = 0
-let empWageArray = new Array()
-let empHrsMap = new Map()
-let empWageMap = new Map()
+let totalWorkDays = 0
+let empempHrsAndWageArray = new Array();
 
-function calcDailyWage(dailyHrs){
-  return dailyHrs * WAGE_PER_HOUR
+function calcDailyWage(empHrs){
+  return empHrs * WAGE_PER_HOUR
 }
 
-while(totalHrs <= MAX_HRS_IN_MONTH && WorkingDays < NO_OF_DAYS){
-  WorkingDays++;
+while(totalHrs <= MAX_HRS_IN_MONTH && totalWorkDays < NO_OF_DAYS){
+  totalWorkDays++;
   let empCheck = Math.floor(Math.random() * 10) % 3;
-  let dailyHrs = getWorkingHours(empCheck);
-  totalHrs += dailyHrs
-  dailyWage = calcDailyWage(dailyHrs)
-  empWageArray.push(calcDailyWage(dailyHrs))
-  empHrsMap.set(WorkingDays, dailyHrs)
-  empWageMap.set(WorkingDays, calcDailyWage(dailyHrs))
-}
-console.log(empWageMap);
-
-function totalHours(totalHrs, dailyHrs){
-  return totalHrs + dailyHrs
-}
-
-const totalWage = (total, value) => {return total + value;}
-console.log("Employee Worked for " + Array.from(empHrsMap.values()).reduce(totalHours, 0))
-console.log("Emp Wage for Total Hours: " + Array.from(empWageMap.values()).filter(dailyWage => dailyWage > 0).reduce(totalWage, 0));
-
-let fullWorkingArray = new Array()
-let partWorkingArray = new Array()
-let notWorkingArray = new Array()
-
-empHrsMap.forEach((value, key) => 
+  let empHrs = getWorkingHours(empCheck);
+  totalHrs += empHrs
+  empempHrsAndWageArray.push(
   {
-    if(value == 8) 
-      fullWorkingArray.push(key);
-    else if(value == 4)
-      partWorkingArray.push(key);
-    else
-      notWorkingArray.push(key);
-  }
-)
-console.log("Full Time Working Days : " + fullWorkingArray)
-console.log("Part time Working Days : " + partWorkingArray)
-console.log("Zero Time Working Days : " + notWorkingArray)
+    dayNum: totalWorkDays,
+    dailyHours: empHrs,
+    dailyWage: calcDailyWage(empHrs),
+    toString() {
+      return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours + ' And Wage Earned = ' + this.dailyWage
+    },
+  })
+}
+console.log(empempHrsAndWageArray);
