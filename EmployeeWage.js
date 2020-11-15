6a06fb876c0886e2b1dde48cbaa650b2b4c53cfb
jangle <shubhamjangale0000@gmail.com>
2020-11-15 17:30:08 +0530

    Added : Ability to check the name before printing

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,45 +1,5 @@
 console.log("Welcome to Employee Payroll Javascript")
 
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2
-const WAGE_PER_HOUR = 20
-const MAX_HRS_IN_MONTH = 160
-const NO_OF_DAYS = 20
-function getWorkingHours(empCheck){
-  switch(empCheck){
-    case IS_PART_TIME:
-      return 4;
-    case IS_FULL_TIME:
-      return 8;
-    default:
-      return 0
-  }
-}
-let totalHrs = 0
-let totalWorkDays = 0
-let totalWage = 0
-let empHrsAndWageArray = new Array();
-
-function calcDailyWage(empHrs){
-  return empHrs * WAGE_PER_HOUR
-}
-
-while(totalHrs <= MAX_HRS_IN_MONTH && totalWorkDays < NO_OF_DAYS){
-  totalWorkDays++;
-  let empCheck = Math.floor(Math.random() * 10) % 3;
-  let empHrs = getWorkingHours(empCheck);
-  empHrsAndWageArray.push(
-  {
-    dayNum: totalWorkDays,
-    dailyHours: empHrs,
-    dailyWage: calcDailyWage(empHrs),
-    toString() {
-      return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours + ' And Wage Earned = ' + this.dailyWage
-    },
-  })
-}
-console.log("" + empHrsAndWageArray)
-
 class EmployeePayrollData {
   constructor(...params){
     this.id = params[0]
@@ -58,7 +18,11 @@ class EmployeePayrollData {
     return this._name
   }
   set name(name){
-    this._name = name
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$')
+    if(nameRegex.test(name))
+      this._name = name
+    else
+      throw "Name incorrect"
   }
   get salary() {
     return this._salary
@@ -87,7 +51,11 @@ class EmployeePayrollData {
 }
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 2000)
 console.log(employeePayrollData.toString())
-employeePayrollData.name = "John"
-console.log(employeePayrollData.toString())
+try {
+  employeePayrollData.name = "john"
+  console.log(employeePayrollData.toString())
+} catch (error) {
+  console.error(error)
+}
 let newEmployeePayrollData = new EmployeePayrollData(1, "Mark", 2000, "M", new Date())
 console.log(newEmployeePayrollData.toString())
